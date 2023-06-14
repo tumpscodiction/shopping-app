b6c219fddc835f55b6d875122652d298deb4a3d1
Tumpa Moni <tumpamoni9991@gmail.com>
2023-06-14 19:37:53 +0600

    first commit

---
+++ app/src/main/java/com/android/retailapp/AppController.java
@@ -0,0 +1,40 @@
+
+
+package com.android.retailapp;
+
+import android.app.Application;
+
+import com.android.retailapp.util.PreferenceHelper;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(mailTo = "tumpamoni9991@gmail.com", customReportContent = {
+        ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME,
+        ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+        ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT}, mode = ReportingInteractionMode.TOAST, resToastText = 0)
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private static AppController mInstance;
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        // The following line triggers the initialization of ACRA for crash Log Reposrting
+        if (PreferenceHelper.getPrefernceHelperInstace().getBoolean(
+                this, PreferenceHelper.SUBMIT_LOGS, true)) {
+            ACRA.init(this);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/android/retailapp/domain/api/ProductCategoryLoaderTask.java
@@ -0,0 +1,95 @@
+
+
+package com.android.retailapp.domain.api;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.retailapp.R;
+import com.android.retailapp.domain.mock.FakeWebServer;
+import com.android.retailapp.util.AppConstants;
+import com.android.retailapp.util.Utils;
+import com.android.retailapp.util.Utils.AnimationType;
+import com.android.retailapp.view.activities.ECartHomeActivity;
+import com.android.retailapp.view.adapter.CategoryListAdapter;
+import com.android.retailapp.view.adapter.CategoryListAdapter.OnItemClickListener;
+import com.android.retailapp.view.fragment.ProductOverviewFragment;
+
+/**
+ * The Class ImageLoaderTask.
+ */
+public class ProductCategoryLoaderTask extends AsyncTask<String, Void, Void> {
+
+    private static final int NUMBER_OF_COLUMNS = 2;
+    private Context context;
+    private RecyclerView recyclerView;
+
+    public ProductCategoryLoaderTask(RecyclerView listView, Context context) {
+
+        this.recyclerView = listView;
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+        super.onPreExecute();
+
+        if (null != ((ECartHomeActivity) context).getProgressBar())
+            ((ECartHomeActivity) context).getProgressBar().setVisibility(
+                    View.VISIBLE);
+
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+
+        if (null != ((ECartHomeActivity) context).getProgressBar())
+            ((ECartHomeActivity) context).getProgressBar().setVisibility(
+                    View.GONE);
+
+        if (recyclerView != null) {
+            CategoryListAdapter simpleRecyclerAdapter = new CategoryListAdapter(
+                    context);
+
+            recyclerView.setAdapter(simpleRecyclerAdapter);
+
+            simpleRecyclerAdapter
+                    .SetOnItemClickListener(new OnItemClickListener() {
+
+                        @Override
+                        public void onItemClick(View view, int position) {
+
+                            AppConstants.CURRENT_CATEGORY = position;
+
+                            Utils.switchFragmentWithAnimation(
+                                    R.id.frag_container,
+                                    new ProductOverviewFragment(),
+                                    ((ECartHomeActivity) context), null,
+                                    AnimationType.SLIDE_LEFT);
+
+                        }
+                    });
+        }
+
+    }
+
+    @Override
+    protected Void doInBackground(String... params) {
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        FakeWebServer.getFakeWebServer().addCategory();
+
+        return null;
+    }
+
+}
